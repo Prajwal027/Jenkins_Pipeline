30ff71245a058420ee881346462b46f53cfac070
Prajwal027 <90600292+Prajwal027@users.noreply.github.com>
2024-03-19 17:40:00 +0530

    Update uni_test_delete.py

--- tests/uni_test_delete.py
+++ tests/uni_test_delete.py
@@ -1,6 +1,6 @@
 import unittest, sys
 sys.path.append('/var/lib/jenkins/workspace/Jenkins_miniproject@2')
-from flask_sql1 import app, db, student
+from flask_sql1 import app, db, Student
 
 class TestDelete(unittest.TestCase):
     def setUp(self):
@@ -10,12 +10,12 @@ class TestDelete(unittest.TestCase):
         # Clean up the database after each test
         with app.app_context():
             db.session.rollback()
-            db.session.query(student).delete()
+            db.session.query(Student).delete()
             db.session.commit()
     def test_delete_student(self):
         with app.app_context():
             student_id = 1             #new_student.id
-            existing_student = student.query.get(student_id)
+            existing_student = Student.query.get(student_id)
             self.assertIsNotNone(existing_student, "Student does not exist")
             # Send a POST request to delete the student
             rv = self.app.post(f'/{student_id}/delete')
@@ -23,7 +23,7 @@ class TestDelete(unittest.TestCase):
             self.assertEqual(rv.status_code, 302)  # Assuming redirect on success
             # Verify that the student has been deleted from the database
             with app.app_context():
-                deleted_student = student.query.get(student_id)
+                deleted_student = Student.query.get(student_id)
                 self.assertIsNone(deleted_student, "Student was not deleted from the database")
 
 if __name__ == '__main__':

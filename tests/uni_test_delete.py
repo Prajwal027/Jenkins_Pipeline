5dc17970f4f955c38dac5d0f7a68d66e83651d04
Prajwal027 <90600292+Prajwal027@users.noreply.github.com>
2024-03-20 17:38:38 +0530

    Update uni_test_delete.py

--- tests/uni_test_delete.py
+++ tests/uni_test_delete.py
@@ -2,7 +2,7 @@
 import sys
 import unittest
 sys.path.append('/var/lib/jenkins/workspace/Jenkins_miniproject@2')
-from flask_sql1 import app, db, Student
+from flask_sql1 import app, db, Student # pylint: disable=wrong-import-position
 
 class TestDelete(unittest.TestCase):
     """This class is used to setup, clean up the database after each test"""

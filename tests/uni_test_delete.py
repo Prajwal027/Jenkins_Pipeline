65dd8dd8a2c7e75b810720618228fcb04349d411
Prajwal027 <90600292+Prajwal027@users.noreply.github.com>
2024-03-20 12:23:38 +0530

    Update uni_test_delete.py
    Organized code

--- tests/uni_test_delete.py
+++ tests/uni_test_delete.py
@@ -1,26 +1,30 @@
+"""This is used as test unit for delete() in flask application"""
 import unittest, sys
 sys.path.append('/var/lib/jenkins/workspace/Jenkins_miniproject@2')
 from flask_sql1 import app, db, Student
 
 class TestDelete(unittest.TestCase):
+    """This class is used to setup, clean up the database after each test"""
     def setUp(self):
+        """This is setup function for this test case"""
         app.testing = True
         self.app = app.test_client()
     def tearDown(self):
-        # Clean up the database after each test
+        """Clean up the database after each test"""
         with app.app_context():
             db.session.rollback()
             db.session.query(Student).delete()
             db.session.commit()
     def test_delete_student(self):
+        """This function will delete the selected student from database"""
         with app.app_context():
-            student_id = 1             #new_student.id
+            student_id = 1
             existing_student = Student.query.get(student_id)
             self.assertIsNotNone(existing_student, "Student does not exist")
             # Send a POST request to delete the student
             rv = self.app.post(f'/{student_id}/delete')
             # Check if the student has been deleted successfully
-            self.assertEqual(rv.status_code, 302)  # Assuming redirect on success
+            self.assertEqual(rv.status_code, 302)
             # Verify that the student has been deleted from the database
             with app.app_context():
                 deleted_student = Student.query.get(student_id)

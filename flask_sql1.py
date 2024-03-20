4b3ce5bd71a832d719a5f615df29e5065c8ad2f3
Prajwal027 <90600292+Prajwal027@users.noreply.github.com>
2024-03-20 12:10:52 +0530

    Update flask_sql1.py

--- flask_sql1.py
+++ flask_sql1.py
@@ -1,3 +1,4 @@
+"""Flask application with student management functionalities and database interaction."""
 import os
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -10,10 +11,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-#db.init_app(app)
+
 class Student(db.Model):
+    """
+    This class represents a student record in the database.
+    """
     id = db.Column(db.Integer, primary_key = True)
     firstname = db.Column(db.String(100), nullable = False)
     lastname = db.Column(db.String(100), nullable = False)
@@ -22,20 +25,32 @@ class Student(db.Model):
     time = db.Column(db.DateTime(timezone=True), server_default = func.now())
     bio = db.Column(db.Text)
 
-    def __repr__(self):
-        return f"<student {self.firstname}>"
-
 @app.route('/')
 def index():
+    """
+    Renders the index.html template displaying a list of all students.
+    """
     students = Student.query.all()
     return render_template('index.html', students=students)
 @app.route('/<int:student_id>')
 def stud(student_id):
+    """
+    Renders the student.html template for a specific student.
+    Raises:
+        404: If student with the provided ID is not found.
+    """
     student_for_stud = Student.query.get_or_404(student_id)
     return render_template('student.html', student=student_for_stud)
 
 @app.route('/create', methods=('POST','GET'))
 def create():
+    """
+    Handles form submission for creating a new student record.
+    This function uses Get method to render an html template.
+    Returns:
+        - Redirected to index page if student creation is successful.
+        - Rendered create.html template if request is GET or creation fails.
+    """
     if request.method=='POST':
         firstname1=request.form['firstname']
         lastname=request.form['lastname']
@@ -57,6 +72,11 @@ def create():
 
 @app.route('/<int:student_id>/edit', methods=('POST', 'GET'))
 def edit(student_id):
+    """
+    Handles the editing of a student record. 
+    1. If already present in the database, it will render create.html template.
+    2. If not then return 404 not found.
+    """
     student_for_edit = Student.query.get_or_404(student_id)
     if request.method=='POST':
         firstname=request.form['firstname']
@@ -76,6 +96,11 @@ def edit(student_id):
 
 @app.post('/<int:student_id>/delete')
 def delete(student_id):
+    """
+    Handles the deleting of a student record.
+    1. If the student_id is present will delete it.
+    2. If not then return 404 not found.
+    """
     student_for_delete = Student.query.get_or_404(student_id)
     db.session.delete(student_for_delete)
     db.session.commit()
